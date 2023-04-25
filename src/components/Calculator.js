c23b379084b6e6bd49d1cda60609868b313c0047
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-04-25 09:13:39 -0500

    component calculator
    - Deleted all the vanilla content for React
    - Added a new Component called Calculator
    - Import the component on app.js

---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="calculatorCont">
+      <h2 className="calculatorTitle">Calculator</h2>
+      <ChildComponent />
+    </div>
+  );
+}
+
+function ChildComponent() {
+  return (
+    <div className="calculator">
+      <input className="inputCont" type="text" id="myInput" name="name" pattern="[0-9+\-*/(). ]+" />
+      <div className="buttons">
+        <input className="calculatorButton mathButtonH" type="button" value="AC" />
+        <input className="calculatorButton mathButtonH" type="button" value="+/-" />
+        <input className="calculatorButton mathButtonH" type="button" value="%" />
+        <input className="calculatorButton mathButtonV" type="button" value="÷" />
+        <br />
+        <input className="calculatorButton" type="button" value="7" />
+        <input className="calculatorButton" type="button" value="8" />
+        <input className="calculatorButton" type="button" value="9" />
+        <input className="calculatorButton mathButtonV" type="button" value="x" />
+        <br />
+        <input className="calculatorButton" type="button" value="4" />
+        <input className="calculatorButton" type="button" value="5" />
+        <input className="calculatorButton" type="button" value="6" />
+        <input className="calculatorButton mathButtonV" type="button" value="-" />
+        <br />
+        <input className="calculatorButton" type="button" value="1" />
+        <input className="calculatorButton" type="button" value="2" />
+        <input className="calculatorButton" type="button" value="3" />
+        <input className="calculatorButton mathButtonV" type="button" value="+" />
+        <br />
+        <input className="calculatorButton zero" type="button" value="0" />
+        <input className="calculatorButton" type="button" value="." />
+        <input className="calculatorButton mathButtonV" type="button" value="=" />
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
