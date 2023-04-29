6edd9d376da837b7e66df78f1936c1d26ee2c890
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-04-28 19:50:51 -0500

    quotes
    - Added a functional component that fetch data from an API, with a personal API Key, and it's properly rendered

---
+++ src/components/Quotes.js
@@ -0,0 +1,63 @@
+import './Quotes.css';
+import React, { useState, useEffect } from 'react';
+
+const Quotes = () => {
+  const category = 'imagination';
+  const [quotes, setQuotes] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+          headers: { 'X-Api-Key': 'hOYQVu3+0rKoH3ywYlpISw==4Cs08SC8ztnACe0O' },
+          contentType: 'application/json',
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setQuotes(data);
+      } catch (error) {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [category]);
+
+  if (hasError || quotes.length === 0) {
+    return <div>Something went wrong!</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {quotes.map((quote) => (
+        <div className="quotesCont" key={quote}>
+          <h2 className="quotesTitle">Quotes</h2>
+          <p className="author">
+            <span className="span">Author:</span>
+            {' '}
+            {quote.author}
+          </p>
+          <p className="quotes">
+            <span className="span">Quote:</span>
+            {' '}
+            <q>
+              {quote.quote}
+            </q>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Quotes;
