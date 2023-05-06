123325593626d1762b14ce82b421394b1d69bdaa
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-05-06 13:58:12 -0500

    Jest Snapshot
    - as requested, added jest snapshots to the Logic testing as well

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -2,34 +2,34 @@ import operate from './operate';
 
 describe('operate', () => {
   test('adds two numbers', () => {
-    expect(operate('2', '2', '+')).toBe('4');
+    expect(operate('2', '2', '+')).toMatchSnapshot();
   });
 
   test('subtracts two numbers', () => {
-    expect(operate('3', '2', '-')).toBe('1');
+    expect(operate('3', '2', '-')).toMatchSnapshot();
   });
 
   test('multiplies two numbers', () => {
-    expect(operate('2', '3', 'x')).toBe('6');
+    expect(operate('2', '3', 'x')).toMatchSnapshot();
   });
 
   test('divides two numbers', () => {
-    expect(operate('6', '2', '÷')).toBe('3');
+    expect(operate('6', '2', '÷')).toMatchSnapshot();
   });
 
   test('handles division by zero', () => {
-    expect(operate('1', '0', '÷')).toBe("Can't divide by 0.");
+    expect(operate('1', '0', '÷')).toMatchSnapshot();
   });
 
   test('calculates modulo', () => {
-    expect(operate('7', '3', '%')).toBe('1');
+    expect(operate('7', '3', '%')).toMatchSnapshot();
   });
 
   test('handles modulo with divisor zero', () => {
-    expect(operate('7', '0', '%')).toBe("Can't find modulo as can't divide by 0.");
+    expect(operate('7', '0', '%')).toMatchSnapshot();
   });
 
   test('throws an error for unknown operations', () => {
-    expect(() => operate('1', '2', '^')).toThrow("Unknown operation '^'");
+    expect(() => operate('1', '2', '^')).toThrowErrorMatchingSnapshot();
   });
 });
